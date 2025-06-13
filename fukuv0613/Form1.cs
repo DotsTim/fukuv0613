a3ef9a583dbf69ad7bb62e6ecd7b103785735b49
kurebayashi rento <kurebayashi.rat@gmail.com>
2025-06-13 13:50:54 +0900

    o

--- fukuv0613/Form1.cs
+++ fukuv0613/Form1.cs
@@ -1,10 +1,35 @@
 namespace fukuv0613
 {
+
     public partial class Form1 : Form
     {
+        int vx = -10, vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+            if (label1.Left < 178)
+            {
+                label1.Left -= vx;
+            }
+            else
+            {
+               
+
+
+
+            }
         }
     }
 }
